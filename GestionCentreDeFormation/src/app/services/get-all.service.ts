ef0afaacab43b5615f101072c4bb7c133a16656d
PaulG <p.gaillard@net.estia.fr>
2022-07-04 15:17:44 +0200

    Debut formateur

--- GestionCentreDeFormation/src/app/services/get-all.service.ts
+++ GestionCentreDeFormation/src/app/services/get-all.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Assistant } from '../models/Assistant.model';
+import { Formateur } from '../models/Formateur';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,25 @@ export class GetAllService {
   constructor(private http:HttpClient) { }
 
 
+  // ------------------------------- Formateur ------------------------------------
+  getAllFormateur() {
+    return this.http.get<Formateur[]>('http://localhost:9090/admin/formateur');
+  }
+  getByIdFormateur(id:number) {
+    return this.http.get<Formateur>('http://localhost:9090/admin/formateur/'+id);
+  }
+  insererFormateur(f:Formateur) {
+    return this.http.post('http://localhost:9090/admin/formateur', f);
+  }
+  modifierFormateur(id:number, f:Formateur) {
+    return this.http.put('http://localhost:9090/admin/formateur/'+id, f);
+  }
+  deleteFormateur(id:number) {
+    return this.http.delete('http://localhost:9090/admin/formateur/'+id);
+  }
 
 
+  // ------------------------------- Assistant ------------------------------------
   getAllAssistant(){
     return this.http.get<Assistant[]>('http://localhost:9090/admin/assistants');
   }
