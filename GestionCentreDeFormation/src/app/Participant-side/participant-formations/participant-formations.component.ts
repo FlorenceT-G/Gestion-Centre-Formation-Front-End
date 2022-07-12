8a8c9f19728736e9475924a82567e76024de433b
FlorenceT-G <florence.thomas.g@outlook.fr>
2022-07-12 11:33:00 +0200

    boite de dialogue envoie mail

--- GestionCentreDeFormation/src/app/Participant-side/participant-formations/participant-formations.component.ts
+++ GestionCentreDeFormation/src/app/Participant-side/participant-formations/participant-formations.component.ts
@@ -80,7 +80,10 @@ export class ParticipantFormationsComponent implements OnInit {
   }
 
   certif(idFormation:number) {
-    this.allService.sendMailDiploma(this.userObject.idUtilisateur, idFormation).subscribe()
+    this.allService.sendMailDiploma(this.userObject.idUtilisateur, idFormation).subscribe(
+      res => alert("Un mail vous a été envoyé ! \nVeuillez vérifier votre boîte de réception")
+    )
+    
   }
 
   accueil(){
