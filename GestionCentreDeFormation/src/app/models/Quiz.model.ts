eb163644b9913e21baa47320505fb95c6f25ae93
BFaustini <benjamin.faustini@hotmail.fr>
2022-07-13 12:32:37 +0200

    quiz

--- GestionCentreDeFormation/src/app/models/Quiz.model.ts
+++ GestionCentreDeFormation/src/app/models/Quiz.model.ts
@@ -1,10 +1,13 @@
 import { Formation } from "./Formation"
+import { Question } from "./Question.model"
 
 export class Quiz {
 
     idQuiz!:number
     nbQuestion!:number
     pourcentage!:number
+
+    question!:Question[]
 
     formation!:Formation
 }
